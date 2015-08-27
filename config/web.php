dff81769947af11a679a51881ff2a24b14984943
csdavidson <jumbomonkeyfish@gmail.com>
2015-08-26 22:56:59 -0700

    trying to fix yii/heroku build

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '273e89133cf48cac805afc8027a43e84',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -45,10 +45,16 @@ $config = [
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug']['class'] = ['yii\debug\Module'];
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+    $config['modules']['debug']['allowedIPs'] = ['*']; 
 
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii']['class'] = ['yii\gii\Module'];
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+    $config['modules']['gii']['allowedIPs'] = ['*']; 
 }
 
 return $config;
