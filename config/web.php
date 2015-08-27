32d4372617466940fcba08317c12f186329eabaf
csdavidson <jumbomonkeyfish@gmail.com>
2015-08-26 22:48:10 -0700

    trying to fix yii/heroku build

--- config/web.php
+++ config/web.php
@@ -45,10 +45,10 @@ $config = [
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug']['class'] = 'yii\debug\Module';
+    $config['modules']['debug']['class'] = ['yii\debug\Module'];
 
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii']['class'] = 'yii\gii\Module';
+    $config['modules']['gii']['class'] = ['yii\gii\Module'];
 }
 
 return $config;
